9e49e531a88dda76a14286456b34fadbbbeae1e2
Laszlo Papp <lpapp@kde.org>
2015-01-18 15:38:54 +0000

    Add the necessary main function content to the UI part

--- frontends/blackberry/ui/main.cpp
+++ frontends/blackberry/ui/main.cpp
@@ -1,3 +1,12 @@
-int main()
+#include "mainwindow.h"
+
+#include <bb/cascades/Application>
+
+using namespace bb::cascades;
+
+Q_DECL_EXPORT int main(int argc, char **argv)
 {
+    Application app(argc, argv);
+    MainWindow mainWindow;
+    return Application::exec();
 }
